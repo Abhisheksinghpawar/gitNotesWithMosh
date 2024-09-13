e6805a60b3312a436055a7cf1b056d692086408c
Abhisheksinghpawar <abhisheksinghpawar17@gmail.com>
2024-09-13 15:23:20 -0500

    Adding code to learningBranch

--- CheckGitPull.py
+++ CheckGitPull.py
@@ -1,1 +1,2 @@
 print("This file was created directly to GitHub account. Now I will run git pull in my local to check if it's working properly or not")
+print("This line was added to the learning branch on the local sys")
