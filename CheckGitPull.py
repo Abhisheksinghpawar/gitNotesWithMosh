63f5d3a5fbec423902835f8155a2c082a690ec44
Abhisheksinghpawar <abhisheksinghpawar17@gmail.com>
2024-09-13 15:54:09 -0500

    Sync with branch

--- CheckGitPull.py
+++ CheckGitPull.py
@@ -1,3 +1,4 @@
 print("This file was created directly to GitHub account. Now I will run git pull in my local to check if it's working properly or not")
 print("This line was added to the learning branch on the local sys")
 print("This line was added from the local repo to the main branch")
+print("Sync with the branch")
